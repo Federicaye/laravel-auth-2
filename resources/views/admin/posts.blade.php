41500ed0ee287fea86568b035acda0706fab3144
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-05-31 15:11:58 +0200

    update

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -20,6 +20,7 @@
                     <td><p class="td {{$post->id}}">{{$post->title}} </p><input type="text" name="title" class="display-none edit-post {{$post->id}}" name='title' value="{{ old('title', $post->title) }}" required maxlength="255"> </td>
                     <td><p class="td {{$post->id}}">{{$post->slug}}</p> <input type="text" name="slug" class="display-none edit-post {{$post->id}}" name='slug' value="{{ old('slug', $post->slug) }}" required maxlength="255"> </td>
                     <td><p class="td {{$post->id}}">{{$post->content}}</p> <input type="text" name="content" class="display-none edit-post {{$post->id}}" name='content' value="{{ old('content', $post->content) }}" required maxlength="255"> </td>
+                    <td><button type="submit" class="btn btn-primary"><i class="fa-solid fa-floppy-disk"></i></button></td>
                 </form>
                 <td>
                     <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
@@ -29,7 +30,7 @@
                     </form>
                 </td>
                 <td> <a href="{{route('admin.posts.edit', $post->id)}}" class="btn btn-primary edit-post-button" id="{{$post->id}}"><i class="fa-solid fa-pencil"></i></a></td>
-                <td> <a href="{{route('admin.posts.update', $post->id)}}" class="btn btn-primary" id="{{$post->id}}"><i class="fa-solid fa-floppy-disk"></i></a></td>
+               
 
 
             </tr>
